501ec05eb9ed09ca1f5b722ee64d09e6dc536c7b
Omar Chaabani <omarchaabani22@gmail.com>
2024-06-10 18:22:53 +0200

    feat: new home features

--- src/main/java/it/polimi/tiw/controllers/GetGroupsCreated.java
+++ src/main/java/it/polimi/tiw/controllers/GetGroupsCreated.java
@@ -1,32 +1,26 @@
 package it.polimi.tiw.controllers;
 
+import com.google.gson.Gson;
+import it.polimi.tiw.beans.Group;
 import it.polimi.tiw.beans.User;
+import it.polimi.tiw.dao.GroupDAO;
 import it.polimi.tiw.dao.UserDAO;
 import it.polimi.tiw.utils.ConnectionManager;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.WebContext;
-import org.thymeleaf.templatemode.TemplateMode;
-import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.UnavailableException;
-import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.List;
 
-/**
- * Servlet implementation class CheckLogin
- * Handles login requests by validating user credentials and redirecting to the home page on success.
- */
-@WebServlet(name = "checkLoginServlet", value = "/login")
-@MultipartConfig
-public class CheckLogin extends HttpServlet {
+@WebServlet(name = "getGroupsCreated", value = "/GetGroupsCreated")
+public class GetGroupsCreated extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private Connection connection;
 
@@ -34,7 +28,7 @@ public class CheckLogin extends HttpServlet {
      * @see HttpServlet#HttpServlet()
      * Default constructor.
      */
-    public CheckLogin() {
+    public GetGroupsCreated() {
         super();
     }
 
@@ -58,38 +52,28 @@ public class CheckLogin extends HttpServlet {
      * @throws IOException      if an input or output error is detected when the servlet handles the POST request
      */
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String email = request.getParameter("email");
-        String password = request.getParameter("password");
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("user");
 
+        GroupDAO groupDAO = new GroupDAO(connection);
 
-        if (email == null || email.isEmpty() || password == null || password.isEmpty()) {
-            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            response.getWriter().println("Credenziali vuote o mancanti");
+        List<Group> groups;
+        try {
+            groups = groupDAO.getGroupsByUsername(user.getUsername());
+        } catch (SQLException e) {
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            response.getWriter().println("SQL error: impossibile ricavare i gruppi dell'utente");
             return;
         }
 
-        UserDAO userDAO = new UserDAO(connection);
+        Gson gson = new Gson();
+        String json = gson.toJson(groups);
 
-        try {
-            // Checking validity of credentials
-            User user = userDAO.checkCredentials(email, password);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(json);
 
-            if (user == null) {
-                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-                response.getWriter().println("Email o password errati");
-            } else {
-                request.getSession().setMaxInactiveInterval(300);
-                request.getSession().setAttribute("user", user);
-
-                response.setStatus(HttpServletResponse.SC_OK);
-                response.setContentType("application/json");
-                response.setCharacterEncoding("UTF-8");
-                response.getWriter().println(user.getUsername());
-            }
-        } catch (SQLException e) {
-            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Impossibile validare le credenziali");
-        }
     }
 
     /**
--- src/main/webapp/javacript/home.js
+++ src/main/webapp/javacript/home.js
@@ -1,5 +1,5 @@
 {
-    let groupListCreated, groupListInvited, groupDetails;
+    let groupListCreated, groupListInvited, groupDetails, groupListBodyCreated, groupListBodyInvited;
 
     let pageOrchestrator = new PageOrchestrator();
 
@@ -12,11 +12,131 @@
         }
     }, false);
 
+    function GroupListCreated(groupTitle, groupListBodyCreated) {
+        this.groupListBodyCreated = groupListBodyCreated;
 
+        this.show = function () {
+            let self = this;
+            makeCall("GET", 'GetGroupsCreated', null,
+                function(req) {
+                    if (req.readyState === 4) {
+                        let message = req.responseText;
+                        let errorMessage = document.getElementById("id_error_created");
+
+                        if(req.status === 200) {
+                            let groups = JSON.parse(req.responseText);
+
+                            if(groups.length === 0) {
+                                errorMessage.textContent = "Nessun gruppo creato";
+                                groupListBodyCreated.hidden = true;
+                                return;
+                            }
+
+                            self.update(groups);
+                        } else if(req.status === 403) {
+                            window.location.href = req.getResponseHeader("Location");
+                            window.sessionStorage.removeItem("user");
+                        } else {
+                            errorMessage.textContent = message;
+                        }
+                    }
+                })
+        }
+
+        this.update = function (groups) {
+            this.groupListBodyCreated.innerHTML = "";
+            let self = this;
+
+            groups.forEach(function (group) {
+                let row = document.createElement("tr");
+                let groupTitle = document.createElement("td");
+                let groupDetails = document.getElementById("group_details_invited");
+
+                groupTitle.textContent = group.title;
+                row.appendChild(groupTitle);
+                row.appendChild(groupDetails);
+                self.groupListBodyCreated.appendChild(row); // Aggiungi la riga al corpo della tabella
+            });
+        }
+
+        this.reset = function() {
+            groupTitle.hidden = true;
+        }
+    }
+
+    function GroupListInvited(groupTitle, groupListBodyInvited) {
+        this.groupListBodyInvited = groupListBodyInvited;
+
+        this.show = function () {
+            let self = this;
+            makeCall("GET", 'GetGroupsInvited', null,
+                function(req) {
+                    if (req.readyState == 4) {
+                        let message = req.responseText;
+                        let errorMessage = document.getElementById("id_error_invited");
+
+                        if(req.status === 200) {
+                            let groups = JSON.parse(req.responseText);
+
+                            if(groups.length === 0) {
+                                errorMessage.textContent = "Nessun gruppo in cui sei invitato";
+                                groupListBodyInvited.hidden = true;
+                                return;
+                            }
+
+                            self.update(groups);
+                        } else if(req.status === 403) {
+                            window.location.href = req.getResponseHeader("Location");
+                            window.sessionStorage.removeItem("user");
+                        } else {
+                            errorMessage.textContent = message;
+                        }
+                    }
+                })
+        }
+
+        this.update = function (groups) {
+            this.groupListBodyInvited.innerHTML = "";
+            let self = this;
+
+            groups.forEach(function (group) {
+                let row = document.createElement("tr");
+                let groupTitle = document.createElement("td");
+                let groupDetails = document.getElementById("group_details_invited");
+
+                groupTitle.textContent = group.title;
+                row.appendChild(groupTitle);
+                row.appendChild(groupDetails)
+                self.groupListBodyInvited.appendChild(row); // Aggiungi la riga al corpo della tabella
+            });
+        }
+
+        this.reset = function() {
+            groupTitle.hidden = true;
+        }
+    }
 
     function PageOrchestrator() {
         this.start = function () {
-            console.log("user is logged");
+            document.getElementById("id_username").textContent = window.sessionStorage.getItem("user");
+
+            // creating the group list created by the user
+            groupListCreated = new GroupListCreated(
+                document.getElementById("group_title_created"),
+                document.getElementById("groupListBodyCreated")
+            );
+
+            groupListCreated.reset();
+            groupListCreated.show();
+
+            // creating the group list invited
+            groupListInvited = new GroupListInvited(
+                document.getElementById("group_title_invited"),
+                document.getElementById("groupListBodyInvited")
+            );
+
+            groupListInvited.reset();
+            groupListInvited.show();
         }
         this.refresh = function () {
             console.log("refreshed");
